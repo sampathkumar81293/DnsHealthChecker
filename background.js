eda5502542e37bb9c9724b2b6c05df4c641cdb9b
Kilaparthi Sampath Kumar <Kilaparthi.SampathKumar@stonybrook.edu>
2018-11-09 19:06:06 -0500

    Alerts the user in case of DNS spoofing

--- background.js
+++ background.js
@@ -1,4 +1,71 @@
+dns = {};
 
+chrome.storage.local.get('DNScache' ,
+    function(data)
+    {
+        dns  = data.DNScache || {};
+        //dns = {};
+    }
+);
+
+
+chrome.webRequest.onBeforeRequest.addListener(function(params)
+{
+    console.log(params);
+    console.log( /^(?:(\w+):)?\/\/([^\/\?#]+)/.exec(params.url));
+    var hostdata  = parseurl(params.url);
+    if (!hostdata) return;
+    var data = dns[hostdata.host];
+    if (!data || !data.ips.length) return;
+    var ip = data.ips[data.ips.length -1];
+    // extend this
+}, {
+    urls : ["<all_urls>"]
+},
+    ["blocking"]
+);
+
+function CacheDNS(host , ip){
+    if (!host) return;
+    var cachedata = dns[host];
+    if (!cachedata || !cachedata.ips)
+    {
+        console.log("New entry to DNS cache");
+        cachedata = {
+            ips : []
+        };
+        dns[host] = cachedata;
+    }
+    if (!cachedata.ips.includes(ip)){
+        console.log("Adding DNS entry in Cache"); 
+        cachedata.ips.push(ip);
+        }
+
+    chrome.storage.local.set(
+        {
+            'DNScache' : dns
+        }
+    );
+}
+
+function PresentinCacheDNS(host , ip){
+    if (!host) return;
+    var cachedata = dns[host];
+    var result = {
+        first : !cachedata || !cachedata.ips 
+        //second : true
+    };
+    if (!cachedata || !cachedata.ips)
+    {
+        console.log("adding ip entry to DNS cache");
+        cachedata = {
+            ips : []
+        };
+        dns[host] = cachedata;
+    }
+    result.second = cachedata.ips.includes(ip) ;
+    return result;
+}
 
 chrome.webRequest.onCompleted.addListener(function(params)
 {
@@ -13,9 +80,20 @@ chrome.webRequest.onCompleted.addListener(function(params)
     }
     if (ip) 
     {
-        console.log("Adding resolved DNS entry in Cache");
-        // check if this DNS is diffirent from previous DNS resolution
-        // CacheDNS(hostdata.host , ip)
+        //ip = "172.217.11.14";
+        var ispresent = PresentinCacheDNS(hostdata.host , ip);
+        console.log("Is present : ", ispresent);
+        if (!ispresent.first && !ispresent.second)
+        {
+            alert("DNS Spoof Alert");
+            return;
+        }
+        CacheDNS(hostdata.host , ip);
+        console.log("Logging cached dns values");
+        console.log( Object.keys(dns).length);
+        console.log(hostdata.host);
+        console.log( dns[hostdata.host] );
+        // check if this DNS is different from previous DNS resolution
     }
 }, {
     urls : ["<all_urls>"]
